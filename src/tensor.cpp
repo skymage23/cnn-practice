90bf27edf2a322834850669cc29f101643b0226d
skymage23 <skymage23.cn@gmail.com>
2024-11-12 15:50:57 -0600

    Actual constructor work.

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,42 +1,36 @@
+#include <cstdlib>
+#include <exceptions.hpp>
 #include <tensor.hpp>
 
 namespace cnn_practice {
     #define TENSOR_CNSRT(x) template <typename T> __Tensor<T>::x    
     #define TENSOR_BASE_RET(x,y) template<typename T> x __Tensor<T>::y
-/*
-    MATRIX_CNSRT(__Tensor(int dimensionality)){
 
+    TENSOR_CNSTR(__Tensor<T>(int rank, const int const* shape)){
+        int temp = 1;
+        void *ptr_temp = nullptr;
+
+        if (rank == 0){
+            throw exceptions::ArgumentError("rank cannot be 0");
+        }
+
+        if (shape == nullptr) {
+            throw exceptions::ArgumentError("shape cannot be NULL");
+        }
+        this -> rank = rank
+        this -> shape = shape; //Do I need a cast?
+
+        //Calculate the total amount of memory we need to allocate
+        //so we that we only need to call malloc once.
+        for (int i = 0; i < rank; i++){
+            temp *= shape[i];
+        }
+
+        ptr_temp = malloc(sizeof(T)*temp);
+        if (ptr_temp == nullptr) {
+            throw exceptions::OutOfMemoryException();
+        }
+        
+        this -> data = ptr_temp;
     }
-
-    MATRIX_CNSRT(~__Tensor()){
-
-    }
-
-    MATRIX_BASE_RET(int, get_dimension_size_count(int dimension)){
-
-    }
-
-    #define INTERIM_MATRIX_CNSRT(x) template <typename T> __InterimTensor<T>::x
-    #define INTERIM_MATRIX_BASE_RET(x,y) template <typename T> x __InterimTensor<T>::y
-
-    INTERIM_MATRIX_CNSRT(__InterimTensor(__Tensor<T>& rvalue)){
-
-    }
-
-    INTERIM_MATRIX_CNSRT(~__InterimTensor()){
-
-    }
-
-    INTERIM_MATRIX_BASE_RET(__InterimTensor<T>, operator[](int index)){
-
-    }
-
-    INTERIM_MATRIX_BASE_RET(T, operator[](int index)){
-
-    }
-
-    INTERIM_MATRIX_BASE_RET(Tensor<T>, operator[](int index)){
-
-    }
-*/
 };
